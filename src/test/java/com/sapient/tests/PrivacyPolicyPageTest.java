907e32c30fbe9dbb6e07e5d463ad5c8c088ae445
sanjaybhati87 <sanjaythakur287@gmail.com>
2017-08-26 14:37:11 +0530

    Checking conflict for Master

--- src/test/java/com/sapient/tests/PrivacyPolicyPageTest.java
+++ src/test/java/com/sapient/tests/PrivacyPolicyPageTest.java
@@ -137,7 +137,7 @@ public class PrivacyPolicyPageTest extends SeleniumTestPlan {
 
 	 	System.out.println("Sanjay Singh bhati");
 	 	System.out.println("Rajni Bhati2");
-	 	System.out.println("Sanjay Singh bhati2");
+	 	System.out.println("Sanjay Singh bhati6");
 
 		 
 	 }
