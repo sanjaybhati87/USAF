45148e05774d790847d90183be7a8bbe192a765b
sanjaybhati87 <sanjaythakur287@gmail.com>
2017-08-26 14:50:20 +0530

    Testing the deleted commits

--- src/test/java/com/sapient/tests/Test2.java
+++ src/test/java/com/sapient/tests/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a try message ");
+		System.out.println("This is a try message ");
+		System.out.println("This is a try message ");
 
 	}
 
